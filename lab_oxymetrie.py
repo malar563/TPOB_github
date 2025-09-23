f47c37a4ba71d35264601b06bef19f0e1331889f
Marylise Larouche <MALAR563@ulaval.ca>
2025-09-22 20:24:25 -0400

    Calcul SpO2 py

--- lab_oxymetrie.py
+++ lab_oxymetrie.py
@@ -1,1 +1,18 @@
-print("Allo Aurélie")
+import pandas as pd
+import numpy as np
+
+
+df = pd.read_csv('jspkoi.csv')
+
+array_1 = np.array([])/1023
+array_1 = np.array([])/1023
+
+# Comment trouver ces données? La plus haute et la plus basse de toute l'array ou bien pour un cycle?
+IminR, ImaxR = 0, 0
+IminIR, ImaxIR = 0, 0
+
+r=np.log(IminR/ImaxR)/np.log(IminIR/ImaxIR)
+
+SpO2 = ((0.81-0.18)*r)/(0.81-0.08+((0.29-0.18)*r))
+
+# Mettre comment calculer notre fréquence cardiaque.
