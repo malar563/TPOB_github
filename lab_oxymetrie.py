399162eb5b6dbbf0270a6532a9b8f28decf7055e
Marylise Larouche <MALAR563@ulaval.ca>
2025-09-26 10:03:02 -0400

    SpO2 trouvé mais clairement mauvais chiffre

--- lab_oxymetrie.py
+++ lab_oxymetrie.py
@@ -55,27 +55,39 @@ plt.ylabel("Intensité normalisée [-]", fontsize=14)
 plt.xlabel("Temps [s]", fontsize=14)
 plt.show()
 
-# # Pandas trie la 1ere colonne en ordre croissant et les autres colonnes s'ordonnent pareil
-# pd.DataFrame(np.array([temps_mins.extend(temps_maxs), intensite_mins.extend(intensite_maxs)]), columns=["temps","intensite"])
-t, i = list(time[minimums[0]]), list(ir[minimums[0]])
-t.extend(list(time[maximums[0]]))
-i.extend(list(ir[maximums[0]]))
+ratios_colors = []
+for i, color in enumerate(colors):
+    t_min = list(time[minimums[i]])
+    i_min = list(color[minimums[i]])
 
-df_ir = pd.DataFrame(np.array([t, i]).T, columns=["temps","intensite"])
-print(df_ir)
+    t_max = list(time[maximums[i]])
+    i_max = list(color[maximums[i]])
 
-df_ir.sort_values(by="temps")
-ratio_intensite = np.diff(df_ir["intensite"])
+    # Calcul des ratios min / max le plus proche
+    ratios = []
+
+    for t_m, i_m in zip(t_max, i_max):
+        # Trouver le minimum le plus proche en temps
+        idx_closest = np.argmin(np.abs(np.array(t_min) - t_m))
+        i_min_closest = i_min[idx_closest]
+
+        ratios.append(i_min_closest/i_m)
+    
+    ratios_colors.append(ratios)
+
+print(ratios_colors)
+
+def SpO2(list_ratios_IR, list_ratios_R):
+    ratio_R = np.median(list_ratios_R) # IminR/ImaxR
+    ratio_IR = np.median(list_ratios_IR) # IminIR/ImaxIR
+
+    r=np.log(ratio_R)/np.log(ratio_IR)
+    SpO2 = ((0.81-0.18)*r)/(0.81-0.08+((0.29-0.18)*r))
+
+    return SpO2
+
+
+print("SpO2 calculé :", SpO2(ratios_colors[0], ratios_colors[1]))
 
 
 
-# # Comment trouver ces données? La plus haute et la plus basse de toute l'array ou bien pour un cycle?
-# IminR, ImaxR = 0, 0
-# IminIR, ImaxIR = 0, 0
-
-# r=np.log(IminR/ImaxR)/np.log(IminIR/ImaxIR)
-
-
-# SpO2 = ((0.81-0.18)*r)/(0.81-0.08+((0.29-0.18)*r))
-
-# # Mettre comment calculer notre fréquence cardiaque.
