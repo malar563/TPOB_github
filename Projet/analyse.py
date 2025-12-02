9cba58d050ee5cb15e272221a499700337043033
Aurélie Beaudoin <aurelie.beaudoin.1@ulaval.ca>
2025-12-02 14:00:49 -0500

    tiens

--- Projet/analyse.py
+++ Projet/analyse.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 
-data = pd.read_csv("data.csv").to_numpy()
+data = pd.read_csv(r"Projet\no_interference.txt", sep = "\t", skiprows=13, decimal=",").to_numpy()
 n = len(data[:,0])    # pour avoir le nb de points en fréq
 c = 2.998e8     # [m/s]
+
+params, cov = curve_fit("gaussian", data[:,0], data[:,1])
+print(params)
+
 l0 = ...    # fréquence centrale de la source
 
 dw = 2*np.pi*c * (data[-1,0] - data[0,0])/l0
